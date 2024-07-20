b1216166f95fdbb870936aa8612a8ab5c3ddc2a7
ALexus <sumskoj01@gmail.com>
2024-07-20 13:34:49 +0300

    User model

---
+++ src/main/java/ssu/cttkz/model/User.java
@@ -0,0 +1,19 @@
+package ssu.cttkz.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    private String id;
+    private String name;
+    private String userpic;
+    private String email;
+    private String gender;
+    private String locale;
+}
