677b1eb74f389838904c9a10525dcbb3f8ce3442
ALexus <sumskoj01@gmail.com>
2024-07-20 13:34:59 +0300

    JobType service

---
+++ src/main/java/ssu/cttkz/service/JobTypeService.java
@@ -0,0 +1,20 @@
+package ssu.cttkz.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import ssu.cttkz.model.JobType;
+import ssu.cttkz.repository.JobTypeRepository;
+
+import java.util.List;
+
+@Service
+public class JobTypeService {
+
+    @Autowired
+    private JobTypeRepository jobTypeRepository;
+
+    public List<JobType> getAll() {
+        return jobTypeRepository.findAll(Sort.by(Sort.Direction.ASC, "id"));
+    }
+}
