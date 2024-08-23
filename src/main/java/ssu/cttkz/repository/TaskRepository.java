e0a45d348baf1de0f529ed16837673878cab3fef
ALexus <sumskoj01@gmail.com>
2024-08-23 10:22:01 +0300

    findBy

--- src/main/java/ssu/cttkz/repository/TaskRepository.java
+++ src/main/java/ssu/cttkz/repository/TaskRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ssu.cttkz.model.Task;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    Task findByApplicationNumberOriginal(String applicationNumberOriginal);
 }
